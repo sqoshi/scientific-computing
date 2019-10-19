b35b5b4489b1e7fc80c3820c0a637cb7b1b90012
Piotr Popis <piotrpopis98@icloud.com>
2019-10-19 23:05:32 +0200

    almost all

--- list01/exercise7/exercise7.jl
+++ list01/exercise7/exercise7.jl
@@ -1,3 +1,25 @@
 #piotr Popis
-function derivative(x,h)
+function derivative(func,x,h)
+        return ((func(x+h)-func(x))/h)
 end
+
+function f(x)
+return sin(x)+cos(3.0*x)
+end
+
+function f_derivative(x)
+    return cos(x) - 3*sin(3*x)
+end
+
+function printer(p,x)
+
+        d=f_derivative(x)
+        println(p," ",d)
+        for i in 0:p
+                h=Float64(2.0)^(-i)
+                m=derivative(f,x,h)
+                println("p:"m)
+end
+end
+
+printer(54.0,Float64(1.0))
