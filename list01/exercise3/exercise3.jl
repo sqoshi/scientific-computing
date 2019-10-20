fc4e97b5adffb818273398dee22075e27a5c4ef3
Piotr Popis <piotrpopis98@icloud.com>
2019-10-20 19:42:49 +0200

    l1  finished

--- list01/exercise3/exercise3.jl
+++ list01/exercise3/exercise3.jl
@@ -8,11 +8,11 @@ end
 
 
 println("[1, 2]: ")
-uniformity(Float64((0.5)^(52.0)))
+uniformity(Float64((2.0)^(-52.0)))
 println("--------------------------")
 println("[0,5, 1]: ")
-uniformity(Float64((0.5)^(53.0)))#(0.5)^(52.0)*0,5^1
+uniformity(Float64((2.0)^(-53.0)))#(0.5)^(52.0)*0,5^1
 println("--------------------------")
 println("[2, 4]: ")#(0.5)^(52.0)/0,5^1
-uniformity(Float64((0.5)^(51.0)))
+uniformity(Float64((2.0)^(-51.0)))
 println("--------------------------")
