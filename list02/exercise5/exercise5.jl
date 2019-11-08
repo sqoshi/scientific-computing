0276a8f91b3f8d477407f63f0026277f213a2b93
Piotr Popis <piotrpopis98@icloud.com>
2019-11-08 18:30:34 +0100

    xd

--- list02/exercise5/exercise5.jl
+++ list02/exercise5/exercise5.jl
@@ -1,7 +1,9 @@
 #Piotr Popis
 """
 """
-function population_growth(r,p)
+function population_growth(r,p,precision)
+    p=precision(p)
+    r=precision(r)
 for i in 1:40
     p=p+r*p*(1-p)
 end
@@ -9,15 +11,17 @@ println(p)
 end
 """
 """
-function population_growth_round(r,p)
+function population_growth_round(r,p,precision)
+    p=precision(p)
+    r=precision(r)
     for i in 1:40
         p=p+r*p*(1-p)
         if(i==10)
             p=trunc(p,digits=3)
-            println("round: ",p)
+            println("Should be 0.722: ",p)
         end
     end
     println(p)
     end
-population_growth(Float32(3.0),Float32(0.01))
-population_growth_round(Float32(3.0),Float32(0.01))
+population_growth((3.0),(0.01),Float32)
+population_growth_round((3.0),(0.01),Float32)
