86c6d5b9627bc98758d7d59d35b04692953b5167
Piotr Popis <piotrpopis98@icloud.com>
2019-11-08 18:18:45 +0100

    ex5 added

--- list02/exercise5/exercise5.jl
+++ list02/exercise5/exercise5.jl
@@ -1,1 +1,23 @@
-xddddddddddddddd
+#Piotr Popis
+"""
+"""
+function population_growth(r,p)
+for i in 1:40
+    p=p+r*p*(1-p)
+end
+println(p)
+end
+"""
+"""
+function population_growth_round(r,p)
+    for i in 1:40
+        p=p+r*p*(1-p)
+        if(i==10)
+            p=trunc(p,digits=3)
+            println("round: ",p)
+        end
+    end
+    println(p)
+    end
+population_growth(Float32(3.0),Float32(0.01))
+population_growth_round(Float32(3.0),Float32(0.01))
