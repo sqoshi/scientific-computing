5a9dda1e9d70fd3e269bc2eeb451a9e0f59f86e2
Piotr Popis <piotrpopis98@icloud.com>
2019-11-08 23:49:55 +0100

    plots done

--- list02/exercise6/exercise6.jl
+++ list02/exercise6/exercise6.jl
@@ -1,4 +1,5 @@
 #Piotr Popis
+using Plots
 function recursion(x,c)
     return (x^2 + c)
 end
@@ -6,6 +7,7 @@ end
 function repeat_func(times)
     c = Float64[-2,-2,-2,-1,-1,-1,-1]
     x = Float64[1,2,1.99999999999999,1,-1,0.75,0.25]
+    a = Float64[1:40;]
     for i in 1:times
         x[1]=recursion(x[1],c[1])
         x[2]=recursion(x[2],c[2])
@@ -15,7 +17,13 @@ function repeat_func(times)
         x[6]=recursion(x[6],c[6])
         x[7]=recursion(x[7],c[7])
         println(i," : ",x[1]," : ",x[2]," : ",x[3]," : ",x[4]," : ",x[5]," : ",x[6]," : ",x[7])
+        a[i]=x[7]
     end
+    print(a)
+    b = 1:40; y = a;
+    plot(b,y,framestyle = :origin)
+    png("/home/piotr/Documents/scientific-computing/list02/exercise6/plots/plot7.png")
+
 end
 println("----------------------------------------------------")
 repeat_func(40)
