38e5e9953a738daadf0b13733c07402234b2b467
Piotr Popis <piotrpopis98@icloud.com>
2019-12-21 22:29:05 +0100

    matrix and vector from file loading to memory

--- list05/blocksys.jl
+++ list05/blocksys.jl
@@ -1,4 +1,5 @@
 module blocksys
+using SparseArrays
 function getVec(file::String)
 
     b=zeros(0)
@@ -17,5 +18,26 @@ function getVec(file::String)
     return b
 end
 
+function getMatrix(file::String)
+
+    open(file) do f
+        line=split(readline(f))
+        n=parse(Int64,line[1])
+        l=parse(Int64,line[2])
+        A=spzeros(Float64,(n),(n))
+        while !eof(f)
+            line=split(readline(f))
+            i=parse(Int64,line[1])
+            j=parse(Int64,line[2])
+            a=parse(Float64,line[3])
+            A[i,j]=a
+        end
+        return n,l,A
+    end
+end
+
+
 println(getVec("b.txt"))
+println(getMatrix("A.txt"))
+
 end
