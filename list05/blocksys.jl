6376a0d084ea145289034128529cd13462b62768
Piotr Popis <piotrpopis98@icloud.com>
2019-12-21 21:35:48 +0100

    33321

--- list05/blocksys.jl
+++ list05/blocksys.jl
@@ -1,2 +1,21 @@
 module blocksys
+function getVec(file::String)
+
+    b=zeros(0)
+
+    open(file) do f
+    line=(readline(f))
+    n=parse(Float64,line)
+
+    while !eof(f)
+        line=readline(f)
+        n=parse(Float64,line)
+        append!(b,n)
+    end
+
+    end
+    return b
+end
+
+println(getVec("b.txt"))
 end
